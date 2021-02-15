79e1ec023d6d126919b0b445d5db5224cc6751f0
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-15 15:22:40 -0500

    Formulas: Simple moving average as function

--- formulas.js
+++ formulas.js
@@ -1,1 +1,8 @@
 // Simple moving Average
+// SMA = (period sum) / N where period sum is the sum of data during the N period
+
+function sma(data) {
+    return (  
+   ( data.reduce(( accumulator, currentValue ) => accumulator + currentValue, 0 ) ) / data.length
+    )
+}
